7e4dc842a7d93cfe35567a1b77d9bada7662011e
Alex Forbes <alforbes@gumtree.com>
2015-11-18 20:55:28 +0000

    Move Flask-Testing to tests_require

--- setup.py
+++ setup.py
@@ -4,27 +4,32 @@
 from setuptools import setup, find_packages
 import multiprocessing  # nopep8
 
-setup(name='orlo',
-      version='0.0.1',
-      description='Deployment data capture API',
-      author='Alex Forbes',
-      author_email='alforbes@ebay.com',
-      license='GPL',
-      long_description=open('README.md').read(),
-      url='https://github.com/eBayClassifiedsGroup/orlo',
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=[
-              'arrow',
-              'Flask',
-              'Flask-SQLAlchemy',
-              'Flask-Testing',
-              'Flask-Migrate',
-              'gunicorn',
-              'psycopg2',
-              'pytz',
-              'sphinxcontrib-httpdomain',
-              'sqlalchemy-utils',
-      ],
-      test_suite='tests',
-      )
+setup(
+    name='orlo',
+    version='0.0.1',
+    description='Deployment data capture API',
+    author='Alex Forbes',
+    author_email='alforbes@ebay.com',
+    license='GPL',
+    long_description=open('README.md').read(),
+    url='https://github.com/eBayClassifiedsGroup/orlo',
+    packages=[
+        'orlo',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'Flask-Migrate',
+        'Flask-SQLAlchemy',
+        'arrow',
+        'gunicorn',
+        'psycopg2',
+        'pytz',
+        'sphinxcontrib-httpdomain',
+        'sqlalchemy-utils',
+    ],
+    tests_require=[
+        'Flask-Testing',
+    ],
+    test_suite='tests',
+)
