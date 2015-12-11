6ba9d724d965ff238cd074384b0838902cf5188f
Alex Forbes <alforbes@gumtree.com>
2015-12-11 11:03:28 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import multiprocessing  # nopep8
 
 setup(
     name='orlo',
-    version='0.0.3',
+    version='0.0.4',
     description='Deployment data capture API',
     author='Alex Forbes',
     author_email='alforbes@ebay.com',
